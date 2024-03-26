b908f5774522f16a7fedb0d7fd88522ea2eb8c22
goose <purification1997@gmail.com>
2024-03-26 07:59:37 +0700

    Release v0.3.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Orange.MixProject do
   use Mix.Project
 
   # The CI workflow depends on this
-  @version "0.3.0"
+  @version "0.3.1"
   @source_url "https://github.com/Goose97/orange"
 
   def project do
