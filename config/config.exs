9cd0ab91d8eccfaed0239821078886ba93a97f2c
goose <purification1997@gmail.com>
2025-03-06 08:47:46 +0700

    chore: log to stdout during test

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,3 @@
 import Config
 
-config :logger, backends: [{LoggerFileBackend, :file_log}]
-
-config :logger, :file_log,
-  path: "log/orange.log",
-  level: :info,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
 import_config "#{config_env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,11 @@
 import Config
 
+config :logger, backends: [{LoggerFileBackend, :file_log}]
+
+config :logger, :file_log,
+  path: "log/orange.log",
+  level: :info,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
 config :rustler_precompiled, force_build_all: true
